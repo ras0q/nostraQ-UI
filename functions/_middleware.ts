7bed788d70008414c2d398b1c45a0b31ca5a4690
ras0q <ras0q@users.noreply.github.com>
2024-09-07 19:03:38 +0900

    :bug: fix hostname

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -4,10 +4,12 @@
 
 import { DEV_SERVER_PROXY_HOST } from '../dev.config'
 
+const devServerProxyHost = new URL(DEV_SERVER_PROXY_HOST).host
+
 export const onRequest: PagesFunction<unknown> = async context => {
   const url = new URL(context.request.url)
   if (url.pathname.startsWith('/api/v3')) {
-    url.host = DEV_SERVER_PROXY_HOST
+    url.host = devServerProxyHost
 
     const response = await fetch(new Request(url), {
       headers: context.request.headers
